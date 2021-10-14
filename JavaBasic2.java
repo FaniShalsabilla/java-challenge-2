68d99d01edd4858ed837448284ad404dce96c639
FaniShalsabilla <fanisalsabilla20@gmail.com>
2021-10-14 14:29:39 +0700

    first commit

---
+++ JavaBasic2.java
@@ -0,0 +1,14 @@
+// tentang if else
+
+public class JavaBasic2 {
+
+    public static void main(String[] args){
+        String number = "100";
+    
+        if(number.equals("100")){
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+    }
